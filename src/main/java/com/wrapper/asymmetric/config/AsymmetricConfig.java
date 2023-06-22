32e64597a7d44d4db3144732ca29f2ac127f3731
Ammar Mansuri <AmmarMansuri@Ammar-MacBook-Pro.local>
2023-06-22 03:05:25 +0200

    Removing Asymmetric

--- src/main/java/com/wrapper/asymmetric/config/AsymmetricConfig.java
+++
@@ -1,9 +0,0 @@
-package com.wrapper.asymmetric.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-import java.util.Set;
-
-@ConfigurationProperties(prefix = "crypto-config.asymmetric-encryption")
-public record AsymmetricConfig(String defaultAlgo, Set<String> algorithms) {
-}
--- src/main/java/com/wrapper/asymmetric/models/AsymmetricDecryptionResult.java
+++
@@ -1,8 +0,0 @@
-package com.wrapper.asymmetric.models;
-
-import com.wrapper.asymmetric.enums.AsymmetricAlgorithm;
-
-public record AsymmetricDecryptionResult(byte[] plainText, AsymmetricAlgorithm symmetricAlgorithm) {
-
-
-}
